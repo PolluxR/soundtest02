8ae6507f91a8b1e58816b5a34732371e3d7e1149
sesap_d9wa <sesaped@gmail.com>
2021-06-08 12:03:54 +0800

    test

--- recorder.py
+++ recorder.py
@@ -3,7 +3,7 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# import required module
+# import required modul
 from threading import *
 import sounddevice as sd
 from scipy.io.wavfile import write
